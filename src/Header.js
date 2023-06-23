be1d0e23fbf66a9c7219aa0015941f428fb98ab5
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-06-23 09:00:37 +0000

    Header complete

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Header.css'
 import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton } from '@mui/material';
+import {Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AppsIcon from '@mui/icons-material/Apps';
+import TuneIcon from '@mui/icons-material/Tune';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
   return (
@@ -9,13 +14,33 @@ function Header() {
         <div className="header__left">
             <IconButton>
               <MenuIcon/>
-            </IconButton>
+            </IconButton> 
+            <img src='https://logodownload.org/wp-content/uploads/2018/03/gmail-logo.png' alt=''/>
         </div>
         <div className="header__middle">
-
+           <IconButton> 
+              <SearchIcon/>
+            </IconButton>
+            <input placeholder='Search mail' type='text' />
+            <TuneIcon className="header__inputCaret"/>
         </div>
         <div className="header__right">
 
+             <IconButton>
+              <HelpOutlineIcon/>
+            </IconButton>
+            
+            <IconButton>
+             <SettingsIcon/>
+            </IconButton>
+
+            <IconButton>
+             <AppsIcon/>
+            </IconButton>
+
+            
+
+            <Avatar/>
         </div>
     </div>
   )
