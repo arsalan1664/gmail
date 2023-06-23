e775e3a9c467e95e2b1242806b32cdad79bb4bb7
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-06-23 10:07:55 +0000

    sidebar incomplete

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Sidebar.css"
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import SidebarOption from './SidebarOption';
+import InboxIcon from '@mui/icons-material/Inbox';
+
+
+function Sidebar (){
+    return(
+        <div className="sidebar">
+            <Button startIcon={<AddIcon fontSize='Large'/> } className="sidebar__compose">Compose</Button>
+
+            <SidebarOption Icon={<InboxIcon/>} title='Inbox' number={56}/>
+        </div>
+    )
+}
+
+export default Sidebar
