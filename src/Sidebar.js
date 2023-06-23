fff3a2bfa8d3b7c58cfb1ae2c9cd617133a61b51
Arsalan Ali <arsalan1664@gmail.com>
2023-06-23 12:48:36 +0000

    sidebar style complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,12 @@ import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import SidebarOption from './SidebarOption';
 import InboxIcon from '@mui/icons-material/Inbox';
+import StarIcon from '@mui/icons-material/Star';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import NoteIcon from '@mui/icons-material/Note';
 
 
 function Sidebar (){
@@ -11,7 +17,14 @@ function Sidebar (){
         <div className="sidebar">
             <Button startIcon={<AddIcon fontSize='Large'/> } className="sidebar__compose">Compose</Button>
 
-            <SidebarOption Icon={<InboxIcon/>} title='Inbox' number={12}/>
+            <SidebarOption Icon={<InboxIcon/>} title='Inbox' number={56} selected={true}/>
+            <SidebarOption Icon={<StarIcon/>} title='Stared' number={22}/>
+            <SidebarOption Icon={<AccessTimeIcon/>} title='Snoozed' number={12}/>
+            <SidebarOption Icon={<LabelImportantIcon/>} title='Important' number={36}/>
+            
+            <SidebarOption Icon={<NearMeIcon/>} title='Sent' number={7}/>
+            <SidebarOption Icon={<NoteIcon/>} title='Draft' number={16}/>
+            <SidebarOption Icon={<ExpandMoreIcon/>} title='More' number={68}/>
         </div>
     )
 }
