0339ef13b174957b6bd5be2f5423b48f2ef6bc24
Arsalan Ali <arsalan1664@gmail.com>
2023-06-24 08:19:14 +0000

    emailRow making start

---
+++ src/EmailRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EmailRow.css'
+
+function EmailRow({id,title,subject,discribtion,time}) {
+  return (
+    <div className='emailRow'>
+        
+    </div>
+  )
+}
+
+export default EmailRow
