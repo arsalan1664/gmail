7e9549350fcdafbe27d4556c57731a11e95012bc
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-06-24 08:53:51 +0000

    commit

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './EmailRow.css'
+import { CheckBox } from '@mui/icons-material'
+import StarIcon from '@mui/icons-material/Star';
+import { IconButton } from '@mui/material';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
 
-function EmailRow({id,title,subject,discribtion,time}) {
+function EmailRow({ id, title, subject, discribtion, time }) {
   return (
     <div className='emailRow'>
-        
+      <div className="emailRow_options">
+        <CheckBox />
+        <IconButton>
+          <StarIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantIcon />
+        </IconButton>
+      </div>
+      <div className="emailRow__title">
+
+      </div>
+      <div className="emailRow__message">
+
+      </div>
+      <div className="emailRow__discrip">
+      </div>tion
     </div>
   )
 }
